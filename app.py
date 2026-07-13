4346f77d315b8335c45a2c0bdaf53af21f3c2576
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-13 22:36:00 +0530

    Builds SplitBill.

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from routers import auth_routers, friends_routers, expense_routers
+from exception_handling.exception_handlers import handlers
+
+app = FastAPI()
+
+# back-end
+app.include_router(auth_routers.auth_router)
+app.include_router(friends_routers.friends_router)
+app.include_router(expense_routers.expense_router)
+
+# exception handling
+handlers(app)
+
