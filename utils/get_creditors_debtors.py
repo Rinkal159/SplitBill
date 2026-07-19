dcf52a87158b6a39d970b736573ab10bb40cf179
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-19 10:49:31 +0530

    This is an important change - during calculation of who's the creditor and debtor, if the user is a debtor then determine whether that debtor had settled up his previous debts, if yes then check after clearing up debts if still his balance is less than zero then only append to debtors list, and after calculating debtors and creditors, balance the creditors meaning if a debtor had settled up some amount earlier then creditor too got settled up that is why descresing his lending amount.

--- utils/get_creditors_debtors.py
+++ utils/get_creditors_debtors.py
@@ -1,7 +1,40 @@
-def get_creditors_debtors(splits, creditors, debtors):
+from decimal import Decimal
+
+def get_creditors_debtors(splits, creditors, debtors, settlement_groups):
+    balance_creditors = {}
+    
     for split in splits:
         balance = split.paid_amount - split.share_amount
         if balance > 0:
             creditors.append({"user": split.user, "balance": balance})
+            
         elif balance < 0:
-            debtors.append({"user": split.user, "balance": balance})
+            existed_settlement = settlement_groups.get(split.id, [])
+            
+            total_amount_settled = Decimal("0")
+            
+            if existed_settlement:
+                for settlement_split in existed_settlement:
+                    total_amount_settled += settlement_split.amount_settled
+                
+                    creditor_id = settlement_split.settlement.to_user
+                    balance_creditors[creditor_id] = balance_creditors.get(creditor_id, Decimal("0")) + settlement_split.amount_settled
+                
+            
+            balance += total_amount_settled
+            
+            if balance < 0:
+                debtors.append({"user": split.user, "balance": balance})
+                
+    
+    for c in creditors:
+        remaining = c["balance"] - balance_creditors.get(
+            c["user"].id,
+            Decimal("0")
+        )
+        
+        if remaining > 0:
+            c["balance"] = remaining
+        else:
+            c["balance"] = Decimal("0")
+        
