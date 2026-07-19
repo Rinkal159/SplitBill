b0964f3e32977211abb5691f8736212bc7e0f1f8
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-19 10:40:25 +0530

    Wires up settlement_router.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import auth_routers, friends_routers, expense_routers
+from routers import auth_routers, friends_routers, expense_routers, settlements_router
 from exception_handling.exception_handlers import handlers
 
 app = FastAPI()
@@ -8,6 +8,7 @@ app = FastAPI()
 app.include_router(auth_routers.auth_router)
 app.include_router(friends_routers.friends_router)
 app.include_router(expense_routers.expense_router)
+app.include_router(settlements_router.settlements_router)
 
 # exception handling
 handlers(app)
