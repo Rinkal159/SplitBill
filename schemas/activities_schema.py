80b6cf3e946d8198a84a4d2b47f922a82bb96e48
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-22 17:23:19 +0530

    Creates schema for PaginatedActivitiesResponse.

--- schemas/activities_schema.py
+++ schemas/activities_schema.py
@@ -2,18 +2,29 @@ from pydantic import BaseModel, Field, ConfigDict
 from datetime import datetime
 from decimal import Decimal
 
+
 class Base(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    
-    
+
+
 class UserDetail(Base):
     id: int
     name: str
     profile_picture: str
-    
+
+
 class ActivitiesResponse(Base):
     type: str
     action: str
-    performed_by: UserDetail
+    user: UserDetail
+    performed_by_me: bool
     performed_at: datetime
     amount_settled: Decimal | None
+
+
+class PaginatedActivitiesResponse(Base):
+    activities: list[ActivitiesResponse]
+    page: int
+    skip: int
+    limit: int
+    has_more: bool
