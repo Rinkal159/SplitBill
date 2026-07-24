f9f4a70acf9d587bd8c1400e9cb9853f4ad33b13
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-24 09:55:14 +0530

    Wires up group_router.

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from routers import (
     expense_routers,
     settlements_routers,
     history_routers,
-    activities_routers
+    activities_routers,
+    group_routers
 )
 from exception_handling.exception_handlers import handlers
 
@@ -18,6 +19,7 @@ app.include_router(expense_routers.expense_router)
 app.include_router(settlements_routers.settlements_router)
 app.include_router(history_routers.history_router)
 app.include_router(activities_routers.activites_router)
+app.include_router(group_routers.group_router)
 
 # exception handling
 handlers(app)
