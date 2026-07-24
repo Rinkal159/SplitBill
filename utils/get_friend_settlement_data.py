baa2c54882398af519b84be99722c0872694f07f
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-24 10:08:06 +0530

    The entire logic is tranfered to get_main_settlement_logic because it is needed in another route too so created helper function.

--- utils/get_friend_settlement_data.py
+++ utils/get_friend_settlement_data.py
@@ -8,6 +8,7 @@ from utils.get_expense_groups import get_expense_groups
 from utils.get_creditors_debtors import get_creditors_debtors
 from decimal import Decimal
 from utils.get_settlement_groups import get_settlement_groups
+from utils.get_main_settlement_logic import get_main_settlement_logic
 
 from model import Friends, ExpenseSplits, User
 
@@ -41,60 +42,12 @@ async def get_friend_settlement_data(
     expense_ids = expenses_you_and_friend_involved.scalars().all()
 
     expense_groups = await get_expense_groups(
-        expense_ids=expense_ids, db=db, wantSorted=True
+        expense_ids=expense_ids, db=db, newest_first=True
     )
 
-    settlements = []
-    total_balance = Decimal("0")
-
-    for splits in expense_groups:
-        settlement_groups = await get_settlement_groups(splits, db)
-        expense = splits[0].expense
-
-        creditors = []
-        debtors = []
-        get_creditors_debtors(splits, creditors, debtors, settlement_groups)
-
-        i = 0  # creditor
-        j = 0  # debtor
-
-        settlement = Decimal("0")
-
-        while i < len(creditors) and j < len(debtors):
-            creditor = creditors[i]
-            debtor = debtors[j]
-
-            creditor_balance = creditor["balance"]
-            debtor_balance = abs(debtor["balance"])
-
-            transfer = min(creditor_balance, debtor_balance)
-
-            # you "lent" to friend
-            if (
-                creditor["user"].id == current_user.id
-                and debtor["user"].id == friend_id
-            ):
-                settlement += transfer
-                total_balance += transfer
-
-            # you "borrowed" from friend
-            elif (
-                debtor["user"].id == current_user.id
-                and creditor["user"].id == friend_id
-            ):
-                settlement += -transfer
-                total_balance -= transfer
-
-            creditor["balance"] -= transfer
-            debtor["balance"] += transfer
-
-            if creditor["balance"] <= Decimal("0"):
-                i += 1
-
-            if abs(debtor["balance"]) <= Decimal("0"):
-                j += 1
-
-        settlements.append({"expense": expense, "settlement": settlement})
+    settlements, total_balance = await get_main_settlement_logic(
+        expense_groups=expense_groups, db=db, you=current_user, other=friend_id
+    )
 
     return {
         "friend": friend,
