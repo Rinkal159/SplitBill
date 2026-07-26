deef5e0ebfc8af784618ee9fb7e6f0f10942ba5f
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-26 10:41:51 +0530

    Creates new route that displays all group expenses in which current user is involved.

--- routers/expense_routers.py
+++ routers/expense_routers.py
@@ -11,7 +11,9 @@ from utils.validate_fields import validate_fields
 from utils.create_splits_of_expense import create_expense_splits
 from utils.get_friend_settlement_data import get_friend_settlement_data
 from utils.get_settlement_groups import get_settlement_groups
-from model import ExpenseHistoryAction
+from utils.get_all_expenses_in_which_user_involved import (
+    get_all_expenses_in_which_user_involved,
+)
 
 
 from schemas.expense_schema import (
@@ -22,7 +24,7 @@ from schemas.expense_schema import (
     FriendsSettlementsResponse as FriendsSettlementsResponseSchema,
     UserDetail as UserDetailSchema,
 )
-from model import Expense, ExpenseSplits, Friends, ExpenseHistory, GroupMember, Group
+from model import Expense, ExpenseSplits, Friends, ExpenseHistory, ExpenseHistoryAction, GroupMember, Group
 
 expense_router = APIRouter(prefix="/api/expenses", tags=["Expenses"])
 
@@ -36,18 +38,17 @@ async def add_expense_api(
 ):
 
     participant_ids = set(expense.participants)
-    
+
     # if it is a group expense
     if expense.group_id:
-        
+
         # if group doesn't exist
         result = await db.execute(select(Group).where(Group.id == expense.group_id))
         existed_group = result.scalars().one_or_none()
-        
+
         if not existed_group:
             raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Group not found")
-        
-        
+
     # validate participnats, payments and splits
     await validate_fields(expense.group_id, db, expense, participant_ids, current_user)
 
@@ -63,7 +64,7 @@ async def add_expense_api(
     )
     db.add(new_expense)
     await db.flush()
-    
+
     # creating expense history
     new_expense_history = ExpenseHistory(
         expense_id=new_expense.id,
@@ -73,10 +74,9 @@ async def add_expense_api(
         expense_total_amount=new_expense.total_amount,
         expense_expense_date=new_expense.expense_date,
         action=ExpenseHistoryAction.CREATED,
-        performed_by=current_user.id
+        performed_by=current_user.id,
     )
     db.add(new_expense_history)
-
 
     try:
         # creating splits of that expense
@@ -109,80 +109,56 @@ async def get_all_expenses_api(
     )
     expense_ids = result.scalars().all()
 
-    # sorted in descending order of expense date
-    expense_groups = await get_expense_groups(
-        expense_ids=expense_ids, db=db, newest_first=True
+    settlements = await get_all_expenses_in_which_user_involved(
+        expense_ids=expense_ids, db=db, current_user=current_user
     )
 
-    settlements = []
+    return {"expenses": settlements}
 
-    for splits in expense_groups:
-        settlement_groups = await get_settlement_groups(splits, db)
-        expense = splits[0].expense
 
-        creditors = []
-        debtors = []
-        get_creditors_debtors(splits, creditors, debtors, settlement_groups)
+# * get all group expenses in which you're involved
+@expense_router.get("/groups/{group_id}", response_model=ExpenseResponseSchema)
+async def get_all_group_expenses_api(
+    group_id: int,
+    db: AsyncSession = Depends(get_db),
+    current_user=Depends(get_current_user),
+):
 
-        i = 0  # creditor
-        j = 0  # debtor
+    # group doesn't exist
+    result = await db.execute(select(Group).where(Group.id == group_id))
+    existed_group = result.scalars().one_or_none()
 
-        your_logs = []
-        other_logs = []
-
-        while i < len(creditors) and j < len(debtors):
-            creditor = creditors[i]
-            debtor = debtors[j]
-
-            creditor_balance = creditor["balance"]
-            debtor_balance = abs(debtor["balance"])
-
-            transfer = min(creditor_balance, debtor_balance)
-
-            # you're a creditor then you "lent"
-            if creditor["user"].id == current_user.id:
-                your_logs.append(
-                    {
-                        "to_user": UserDetailSchema.model_validate(debtor["user"]),
-                        "amount": transfer,
-                    }
-                )
-
-            # you're a debtor then you "borrowed"
-            elif debtor["user"].id == current_user.id:
-                your_logs.append(
-                    {
-                        "to_user": UserDetailSchema.model_validate(creditor["user"]),
-                        "amount": -transfer,
-                    }
-                )
-
-            # other settlements
-            else:
-                other_logs.append(
-                    {
-                        "from_user": UserDetailSchema.model_validate(debtor["user"]),
-                        "to_user": UserDetailSchema.model_validate(creditor["user"]),
-                        "amount": -transfer,
-                    }
-                )
-
-            creditor["balance"] -= transfer
-            debtor["balance"] += transfer
-
-            if creditor["balance"] <= Decimal("0"):
-                i += 1
-
-            if abs(debtor["balance"]) <= Decimal("0"):
-                j += 1
-
-        settlements.append(
-            {
-                "expense": expense,
-                "your_settlements": your_logs,
-                "other_settlements": other_logs,
-            }
+    if not existed_group:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Group not found"
         )
+
+    # you're not a member of that group
+    result = await db.execute(
+        select(GroupMember).where(
+            GroupMember.group_id == group_id, GroupMember.user_id == current_user.id
+        )
+    )
+    existed_group_member = result.scalars().one_or_none()
+
+    if not existed_group_member:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You're not a member of this group",
+        )
+
+    # get all group expenses in which you're involved
+    result = await db.execute(
+        select(Expense.id)
+        .distinct()
+        .join(ExpenseSplits)
+        .where(Expense.group_id == group_id, ExpenseSplits.user_id == current_user.id)
+    )
+    expense_ids = result.scalars().all()
+
+    settlements = await get_all_expenses_in_which_user_involved(
+        expense_ids=expense_ids, db=db, current_user=current_user
+    )
 
     return {"expenses": settlements}
 
@@ -317,7 +293,7 @@ async def get_friends_settlements_api(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Cannot fetch expenses with non-friend",
         )
-    
+
     return await get_friend_settlement_data(
         friend_id=friend_id, db=db, current_user=current_user
     )
@@ -348,12 +324,13 @@ async def delete_expense_api(
     # creating expense history
     new_expense_history = ExpenseHistory(
         expense_id=expense_id,
+        group_id=existed_expense.group_id,
         expense_title=existed_expense.title,
         expense_description=existed_expense.description,
         expense_total_amount=existed_expense.total_amount,
         expense_expense_date=existed_expense.expense_date,
-        action="DELETED",
-        performed_by=current_user.id
+        action=ExpenseHistoryAction.DELETED,
+        performed_by=current_user.id,
     )
     db.add(new_expense_history)
 
@@ -386,18 +363,16 @@ async def update_expense_api(
             detail="You're not authorized to perform requested action",
         )
 
-
     # if it is a group expense
     if expense.group_id:
-        
+
         # if group doesn't exist
         result = await db.execute(select(Group).where(Group.id == expense.group_id))
         existed_group = result.scalars().one_or_none()
-        
+
         if not existed_group:
             raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Group not found")
-        
-            
+
     participant_ids = set(expense.participants)
 
     # validate participnats, payments and splits
@@ -409,16 +384,17 @@ async def update_expense_api(
     existed_expense.note = expense.note  # type: ignore[call-args]
     existed_expense.total_amount = expense.total_amount
     existed_expense.expense_date = expense.expense_date
-    
+
     # creating expense history
     new_expense_history = ExpenseHistory(
         expense_id=existed_expense.id,
+        group_id=existed_expense.group_id,
         expense_title=existed_expense.title,
         expense_description=existed_expense.description,
         expense_total_amount=existed_expense.total_amount,
         expense_expense_date=existed_expense.expense_date,
-        action="UPDATED",
-        performed_by=current_user.id
+        action=ExpenseHistoryAction.UPDATED,
+        performed_by=current_user.id,
     )
     db.add(new_expense_history)
 
