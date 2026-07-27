61d1ed596c273a4f997f7d32b14913a25e953feb
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-27 18:40:16 +0530

    New name.

--- utils/hash_password.py
+++
@@ -1,9 +0,0 @@
-from pwdlib import PasswordHash
-
-pwd_context = PasswordHash.recommended()
-
-def hash(password: str) -> str:
-    return pwd_context.hash(password)
-
-def verify(plain_password: str, db_password: str) -> bool:
-    return pwd_context.verify(plain_password, db_password)
