8641a1a470a024c5a81fea4c7f1a82ca5f17a862
rinkal159 <rinkalsingapuri@gmail.com>
2026-07-27 18:39:18 +0530

    Send password reset OTP to user via mail.

---
+++ services/email_generation.py
@@ -0,0 +1,25 @@
+from fastapi_mail import FastMail, MessageSchema, MessageType
+from services.email_config import config
+
+async def send_mail(email, otp):
+    message = MessageSchema(
+        subject="Password Reset",
+        recipients=[email],
+        body=f"""
+Hello,
+
+Your SplitBill password reset verification code is:
+
+{otp}
+
+This code expires in 10 minutes.
+
+If you didn't create this account, please ignore this email.
+""",
+        subtype=MessageType.plain,
+    )
+    
+    mail = FastMail(config)
+    await mail.send_message(message)
+    
+    
