6fc34e5ac23c665c6c6dc26cdc38ecdd13ef5157
rinkal159 <rinkalsingapuri@gmail.com>
2026-08-11 21:53:45 +0530

    Creates component for login and signup buttons.

---
+++ frontend_splitbill/src/pages/dashboard/LoginAndSignup.jsx
@@ -0,0 +1,8 @@
+export default function LoginAndSignup({value}) {
+  return <a
+    href="#"
+    className="text-md text-slate-500 hover:text-sky-600 transition font-medium hidden sm:block"
+  >
+    {value}
+  </a>;
+}
