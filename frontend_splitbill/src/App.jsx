13be176e9dd525c7e649eb8f5152a0409b2db626
rinkal159 <rinkalsingapuri@gmail.com>
2026-08-15 09:27:59 +0530

    Does many important things, it has parent routes that renders childeren routes, two parent routes are there, one unautheticated and second is authenticated, in authenticated parent, it first checks whether user has looged in, if not then doesn't load childeren route, and in unauthenticated parent, there's protected reset route as well, that checks whether user has reset_token to continue or not.

--- frontend_splitbill/src/App.jsx
+++ frontend_splitbill/src/App.jsx
@@ -1,16 +1,37 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import UnauthenticatedBody from "./layout/UnauthenticatedBody";
+import Index from "./pages/index/Index";
+import Signup from "./pages/auth/Signup";
+import Login from "./pages/auth/Login";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+
+import AuthenticatedBody from "./layout/AuthenticatedBody";
 import Dashboard from "./pages/dashboard/Dashboard";
-import Body from "./layout/Body";
+import ResetPassword from "./pages/auth/ResetPassword";
+import ProtectedResetPassword from "./pages/auth/ProtectedResetPassword";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Body />}></Route>
+          {/* non-authenticated */}
+          <Route element={<UnauthenticatedBody />}>
+            <Route path="/" element={<Index />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route element={<ProtectedResetPassword />}>
+              <Route path="/reset-password" element={<ResetPassword />} />
+            </Route>
+          </Route>
+
+          {/* authenticated */}
+          <Route element={<AuthenticatedBody />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
