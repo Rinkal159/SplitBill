0e887b0a72e56f2505044da5b20022bd7364b1e0
rinkal159 <rinkalsingapuri@gmail.com>
2026-08-15 09:37:28 +0530

    Dont' need that anymore.

--- frontend_splitbill/src/pages/dashboard/CircularProfile.jsx
+++
@@ -1,7 +0,0 @@
-export default function CircularProfile({ value, bgColor }) {
-  return (
-    <div className={`w-8 h-8 rounded-full ${bgColor} border-2 border-white flex items-center justify-center text-xs font-semibold`}>
-      {value}
-    </div>
-  );
-}
