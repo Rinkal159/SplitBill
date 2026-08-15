3afd9d49567c6292e9aee3dc77edbc223de67a6f
rinkal159 <rinkalsingapuri@gmail.com>
2026-08-15 09:22:23 +0530

    Includes protection againt CORS errror.

--- backend_splitbill/app.py
+++ backend_splitbill/app.py
@@ -4,8 +4,21 @@ from backend_splitbill.routers import (
     settlements_routers
 )
 from backend_splitbill.exception_handling.exception_handlers import handlers
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# cors error solution
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 # back-end
 app.include_router(auth_routers.auth_router)
